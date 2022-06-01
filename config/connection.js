d5866f6e1b7577dbfbd78df56e77b83f74280472
Andrea-Rowland <100717857+Andrea-Rowland@users.noreply.github.com>
2022-06-01 09:57:22 -0600

    added routes

--- config/connection.js
+++ config/connection.js
@@ -1,8 +1,10 @@
 // import the sequelize constructor from the library
 const Sequelize = require('sequelize');
 
+require('dotenv').config();
+
 // create connection to our database, pass in your MySQL information for username and password
-const sequalize = new Sequelize('just_tech_news_db', 'username', 'password', {
+const sequalize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
     host: 'localhost',
     dialiect: 'mysql',
     port: 3306
