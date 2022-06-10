05c7e71bf2e81098fe1a96c14a1d0b6e5727a4b6
Andrea-Rowland <100717857+Andrea-Rowland@users.noreply.github.com>
2022-06-10 10:23:06 -0600

    completed first issue

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,8 +1,10 @@
 const router = require('express').Router();
 const sequelize = require('../config/connection');
-const { Post, User, Comment } = require('../models');
+const { Post, User, Comment, Vote } = require('../models');
 
+// get all posts for homepage
 router.get('/', (req, res) => {
+  console.log('======================');
   Post.findAll({
     attributes: [
       'id',
@@ -27,9 +29,9 @@ router.get('/', (req, res) => {
     ]
   })
     .then(dbPostData => {
-      console.log(dbPostData)
-      // pass a single post object into the homepage template
-      res.render('homepage', dbPostData[0]);
+      const posts = dbPostData.map(post => post.get({ plain: true }));
+
+      res.render('homepage', { posts });
     })
     .catch(err => {
       console.log(err);
